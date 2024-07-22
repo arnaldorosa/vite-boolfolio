07d32f54e3fd031872bcb3fb2deb4106e92c0668
arnaldorosa <159778135+arnaldorosa@users.noreply.github.com>
2024-07-22 18:43:52 +0200

    api called

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { store } from "../store"
+export default {
+  name: "AppMain",
+  data() {
+    return {
+      store,
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
